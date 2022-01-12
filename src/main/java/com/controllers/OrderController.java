699a529e2048d93f7297a4bf05a5166ba1a1e587
Adesanya David <tito@mytransita.com>
2022-01-12 17:35:17 +0100

    Updates

--- src/main/java/com/controllers/OrderController.java
+++ src/main/java/com/controllers/OrderController.java
@@ -1,5 +1,9 @@
 package com.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OrderController {
+    private final OrderService orderService;
 
 }
